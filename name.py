d896b2d2fc092bfb404166f651dd38b45b1958fe
nikit <filippovnikita4@gmail.com>
2019-03-04 00:02:48 +0300

    3 and 5 works

--- name.py
+++ name.py
@@ -1,6 +1,30 @@
-#print ("hello")
-print ("world")
-a = 5
-b = 3
-c = a + b
-print(c)
+def gw(fn):#начальная сегментация текста на слова
+    with open(fn, encoding="utf8") as file: #открываем файл
+        t = file.read() #читаем файл
+    t = t.replace("\n", " ") #убираем пробелы
+    t = t.replace(",", "").replace(".", "").replace("?", "").replace("!", "")#убираем знаки
+    t = t.lower()#возвращает копию строки, в которой все символы в нижнем регистре.
+    w = t.split()#возвращает список всех слов в строке
+    w.sort()#сортировка списка
+    return w
+
+def gwd(w): #получаем словарь из слов
+    words_d = dict()
+    for word in w:#подсчет
+        if word in words_d:
+            words_d[word] = words_d[word] + 1
+        else:
+            words_d[word] = 1
+    return words_d
+
+def main():#функция вывода
+    fn = input("Введите путь к файлу: ")
+    w = gw(fn)
+    words_d = gwd(w)
+    print("Все слова:")
+    for word in words_d:
+        print(word.ljust(10), words_d[word]) #отступ
+    print("Количество слов: %d" % len(w))
+    print("Количество уникальных слов: %d" % len(words_d))
+if __name__ == "__main__":
+    main()
